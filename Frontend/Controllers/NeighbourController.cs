a7cb46c17a667638f3fecc9f9132953b046059e6
tananett <44161956+tananett@users.noreply.github.com>
2021-05-23 05:53:15 +0700

    init

---
+++ Frontend/Controllers/NeighbourController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Frontend.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using Frontend.Services;
+
+namespace Frontend.Controllers
+{
+    [Authorize]
+    public class NeighbourController : Controller
+    {
+        private readonly ILogger<NeighbourController> _logger;
+
+        public IActionResult Index()
+        {
+            ViewData["Page1"] = "select";
+            ViewData["Page2"] = "unselect";
+            ViewData["Page3"] = "unselect";
+            
+            return View();
+        }
+
+    }
+}
